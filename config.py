a6d450a53a53c03b4d839ac76d9cce0bcd6a3938
LympePee <liberispanagiwtis@gmail.com>
2025-06-06 08:31:46 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,2 @@
 # config.py
 
-GMAIL_USER = "liberispanagiwtis@gmail.com"
-GMAIL_APP_PASSWORD = "cwva taog pwaq edmj"
-CC_EMAIL = "ty_technologoi@ocsc.gr"
-TELEGRAM_TOKEN = "7754156506:AAG9BBxlaH5RthKGxqOSMKpE1cYKeDK6_Zo"
-OPENAI_API_KEY = "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"
-GOOGLE_SERVICE_ACCOUNT_FILE = "config_data/google_creds.json"
-GOOGLE_SPREADSHEET_ID = "12WtE8VW-8pPogOJKKztH-rzUCnkKaCpAz-sPxXZh7eo"
